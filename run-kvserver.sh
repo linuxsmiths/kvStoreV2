dd8e1b0c9e7e0262d177866a6be00e032d5b192f
Nagendra S Tomar <natomar@microsoft.com>
2025-12-05 14:37:14 +0000

    start kvserver on both ports

--- run-kvserver.sh
+++ run-kvserver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./build/KVService/KVStoreServer --port 8085 --log-level error --disable-metrics --disable-multi-nic
+numactl --cpunodebind=0 --membind=0 ./build/KVService/KVStoreServer --port 8085 --log-level error --disable-metrics --disable-multi-nic &
+numactl --cpunodebind=1 --membind=1 ./build/KVService/KVStoreServer --port 8086 --log-level error --disable-metrics --disable-multi-nic &
+
+wait
