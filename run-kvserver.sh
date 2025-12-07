4d61fb88f529e4ffc31334beca771f82d4bc6262
Nagendra S Tomar <natomar@microsoft.com>
2025-12-07 02:25:09 +0000

    Run both servers on numa node 1 where the nic is bound.
    We can do this for large VMs where one numa node is not fully sturated.
    For smaller VMs or higher load, put servers on differen numa nodes.

--- run-kvserver.sh
+++ run-kvserver.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-numactl --cpunodebind=0 --membind=0 ./build/KVService/KVStoreServer --port 8085 --log-level error --disable-metrics --disable-multi-nic &
-numactl --cpunodebind=1 --membind=1 ./build/KVService/KVStoreServer --port 8086 --log-level error --disable-metrics --disable-multi-nic &
+#
+# On my VM the nic is bound to numa node 1, also one numa node is sufficient
+# to process the current load.
+# If one numa node is not sufficient then run one server process on each numa
+# node.
+#
+while :; do
+	numactl --cpunodebind=1 --membind=1 ./build/KVService/KVStoreServer --port 8085 --log-level error --disable-metrics --disable-multi-nic &
+	numactl --cpunodebind=1 --membind=1 ./build/KVService/KVStoreServer --port 8086 --log-level error --disable-metrics --disable-multi-nic &
 
-wait
+	wait
+done
